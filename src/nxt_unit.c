d416d38f49d16ae415bdf2e55994391bc7d33c98
Max Romanov <max.romanov@gmail.com>
2019-01-15 23:20:04 +0300

    Removing raw data logging.

--- src/nxt_unit.c
+++ src/nxt_unit.c
@@ -2524,14 +2524,12 @@ nxt_unit_mmap_read(nxt_unit_ctx_t *ctx, nxt_unit_recv_msg_t *recv_msg,
         b->buf.end = b->buf.start + size;
         b->hdr = hdr;
 
-        nxt_unit_debug(ctx, "#%"PRIu32": mmap_read: [%p,%d] %d->%d,(%d,%d,%d)\n"
-                       "%.*s",
+        nxt_unit_debug(ctx, "#%"PRIu32": mmap_read: [%p,%d] %d->%d,(%d,%d,%d)",
                        recv_msg->port_msg.stream,
                        start, (int) size,
                        (int) hdr->src_pid, (int) hdr->dst_pid,
                        (int) hdr->id, (int) mmap_msg->chunk_id,
-                       (int) mmap_msg->size,
-                       (int) size, (char *) start);
+                       (int) mmap_msg->size);
     }
 
     pthread_mutex_unlock(&process->incoming.mutex);
